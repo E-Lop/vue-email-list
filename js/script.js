0fad02e8fbd395fab05d99477e4cd0eb7b006cb7
E-Lop <edmondolopez@gmail.com>
2022-07-04 13:51:46 +0200

    analisi consegna + struttura Vue

---
+++ js/script.js
@@ -0,0 +1,23 @@
+/* Descrizione:
+Attraverso l'apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati. */
+
+// ------------------------------------
+// ANALISI
+// ------------------------------------
+// creo array che conterrà gli indirizi email
+// creo funzione che per 10 volte interroga l'API e fa il push del risultato nell'array delle email
+// stampo il contenuto dell'array nel DOM
+//
+// BONUS
+// lego la stampa dell'array nel DOM a un v-if con lunghezza dell'array = 10
+// ------------------------------------
+
+var app = new Vue({
+  el: '#root',
+  data: {},
+  methods: {},
+});
